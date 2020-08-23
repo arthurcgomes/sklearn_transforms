4c4ad921d13a8554b91c5192e562104f9b703834
arthurcgomes <arthurcgomes@hotmail.com>
2020-08-22 23:42:23 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -19,13 +19,13 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = data.drop(labels=self.columns, axis='columns')
  #         return data.drop(labels=self.columns, axis='columns')
 
-        perfil = data['PERFIL']
-        columns = data.columns
-        x = data.loc[:, data.columns != 'PERFIL'].values
-        min_max_scaler = preprocessing.MinMaxScaler()
-        x_scaled = min_max_scaler.fit_transform(x)
-        data = pd.DataFrame(x_scaled)
-        data = data.join(perfil)
-        data.columns = columns
+#         perfil = data['PERFIL']
+#         columns = data.columns
+#         x = data.loc[:, data.columns != 'PERFIL'].values
+#         min_max_scaler = preprocessing.MinMaxScaler()
+#         x_scaled = min_max_scaler.fit_transform(x)
+#         data = pd.DataFrame(x_scaled)
+#         data = data.join(perfil)
+#         data.columns = columns
 
         return data
