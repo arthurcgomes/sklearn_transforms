fc072b804ed45cbc68b6019ae33fd00fad0b951a
arthurcgomes <arthurcgomes@hotmail.com>
2020-08-22 22:02:30 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -4,20 +4,6 @@ from sklearn import preprocessing
 from sklearn.utils import resample
 
 
-# All sklearn Transforms must have the `transform` and `fit` methods
-class DropColumns(BaseEstimator, TransformerMixin):
-    def __init__(self, columns):
-        self.columns = columns
-        
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        # Primeiro realizamos a cópia do dataframe 'X' de entrada
-        data = X.copy()
-        # Retornamos um novo dataframe sem as colunas indesejadas
-        return data.drop(labels=self.columns, axis='columns')
-        
         
 class Normalize(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
@@ -37,3 +23,20 @@ class Normalize(BaseEstimator, TransformerMixin):
         data.columns = columns
         
         return data
+
+
+
+# All sklearn Transforms must have the `transform` and `fit` methods
+class DropColumns(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+        
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        # Retornamos um novo dataframe sem as colunas indesejadas
+        return data.drop(labels=self.columns, axis='columns')
+        
