88a2ece9a6e2a039e2f1e8d446b2e6afeec963a0
arthurcgomes <arthurcgomes@hotmail.com>
2020-08-22 21:50:10 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -8,12 +8,32 @@ from sklearn.utils import resample
 class DropColumns(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
         self.columns = columns
-
+        
     def fit(self, X, y=None):
         return self
-    
+
     def transform(self, X):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
-        return X.copy().drop(labels=self.columns, axis='columns')
-    
+        return data.drop(labels=self.columns, axis='columns')
+        
+        
+class Normalize(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        # Retornamos um novo dataframe sem as colunas indesejadas
+        perfil = data['PERFIL']
+        columns = data.columns
+        x = data.loc[:, data.columns != 'PERFIL'].values
+        min_max_scaler = preprocessing.MinMaxScaler()
+        x_scaled = min_max_scaler.fit_transform(x)
+        data = pd.DataFrame(x_scaled)
+        data = data.join(perfil)
+        data.columns = columns
+        
+        return data
