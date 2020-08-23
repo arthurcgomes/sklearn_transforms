e47d95f9d718cbca5a9da25e69be915b7e6cd9e1
arthurcgomes <arthurcgomes@hotmail.com>
2020-08-22 23:05:21 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -3,23 +3,7 @@ from sklearn.neural_network import MLPClassifier
 from sklearn import preprocessing
 from sklearn.utils import resample
         
-class Normalize(BaseEstimator, TransformerMixin):
-    def fit(self, X, y=None):
-        return self
-    def transform(self, X):
-        # Primeiro realizamos a cópia do dataframe 'X' de entrada
-        data = X.copy()
-        # Retornamos um novo dataframe sem as colunas indesejadas
-        perfil = data['PERFIL']
-        columns = data.columns
-        x = data.loc[:, data.columns != 'PERFIL'].values
-        min_max_scaler = preprocessing.MinMaxScaler()
-        x_scaled = min_max_scaler.fit_transform(x)
-        data = pd.DataFrame(x_scaled)
-        data = data.join(perfil)
-        data.columns = columns
-        
-        return data
+
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
@@ -31,16 +15,15 @@ class DropColumns(BaseEstimator, TransformerMixin):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
          # Retornamos um novo dataframe sem as colunas indesejadas
-         data = data.drop(labels=self.columns, axis='columns')
+        data = data.drop(labels=self.columns, axis='columns')
  #         return data.drop(labels=self.columns, axis='columns')
 
-         perfil = data['PERFIL']
-         columns = data.columns
-         x = data.loc[:, data.columns != 'PERFIL'].values
-         min_max_scaler = preprocessing.MinMaxScaler()
-         x_scaled = min_max_scaler.fit_transform(x)
-         data = pd.DataFrame(x_scaled)
-         data = data.join(perfil)
-         data.columns = columns
-
-         return data
+        perfil = data['PERFIL']
+        columns = data.columns
+        x = data.loc[:, data.columns != 'PERFIL'].values
+        min_max_scaler = preprocessing.MinMaxScaler()
+        x_scaled = min_max_scaler.fit_transform(x)
+        data = pd.DataFrame(x_scaled)
+        data = data.join(perfil)
+        data.columns = columns
+        return data
